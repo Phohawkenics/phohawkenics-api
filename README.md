243ef4f885b132774de2d35086b06423317db8fa
phohawkenics <leolanuzo@gmail.com>
2017-10-24 20:57:56 -0400

    Initial Commit

---
+++ README.md
@@ -0,0 +1,3 @@
+//// Build & Run
+
+gradlew build && java -jar ./build/libs/phohawkenics-api-0.1.0.jar
---
+++ src/main/java/helloworld/model/Greeting.java
@@ -0,0 +1,23 @@
+package helloworld.model;
+
+/**
+ * Created by Phohawkenics on 2017-10-23.
+ */
+public class Greeting {
+
+    private final long id;
+    private final String content;
+
+    public Greeting(long id, String content) {
+        this.id = id;
+        this.content = content;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public String getContent() {
+        return content;
+    }
+}
